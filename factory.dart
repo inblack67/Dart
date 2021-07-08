3886dc260e5d597d06d9fbb85b3ffe31ac149a8c
ryuzaki <ryuzaki>
2021-07-08 19:58:25 +0530

    factory ctor

---
+++ factory.dart
@@ -0,0 +1,5 @@
+// Use a factory in situations where you don't necessarily want to return a new instance of the class itself. Use cases:
+
+// the constructor is expensive, so you want to return an existing instance - if possible - instead of creating a new one;
+// you only ever want to create one instance of a class (the singleton pattern);
+// you want to return a subclass instance instead of the class itself.
